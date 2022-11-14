a902fd6852af8e6fde04a536312b7a9b5dcb7d91
arkorede <israelashaolu2000@gmail.com>
2022-11-14 23:34:22 +0100

    feat: Hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './queries.css';
 import Header from './Components/Header'
+import Hero from './Components/Hero'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import houses from "./houses.png";
+
+function Hero() {
+  return (
+    <div>
+      <div
+        className="section-hero"
+        style={{ padding: "5rem", marginTop: "7rem" }}
+      >
+        <div className="hero" style={{ marginRight: "3rem" }}>
+          <div className="hero-heading">
+            Rent a <span className="hero-highlight">Place</span> away from
+            <span className="hero-highlight"> Home</span> in the
+            <span className="hero-highlight"> Metaverse</span>
+          </div>
+          <p className="hero-description">
+            we provide you access to luxury and affordable houses in the
+            metaverse, get a chance to turn your imagination to reality at your
+            comfort zone
+          </p>
+          <div className="hero-location input-group input-group-lg">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search for Location"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"
+            />
+            <span
+              className="input-group-text text-white fs-3 px-5 py-4 btn-search"
+              id="inputGroup-sizing-lg"
+            >
+              Search
+            </span>
+          </div>
+        </div>
+        <div className="hero-img">
+          <img
+            src={houses}
+            alt="A group of Houses"
+            className="img-fluid mx-auto d-block"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
